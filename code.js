a8f04a6cb7f6887a998970ab8c12d6d6a00e6e79
cassidymanuel01 <cassidymanuel.59@gmail.com>
2022-06-17 14:54:56 +0200

    'changes'

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
-const array = [1, 2, 3];
-  const iterator = array.values();
+const numbers = [1, 2, 3];
+  const iterator = numbers.values();
   let one = iterator.next().value;
   let two = iterator.next().value;
-console.log(one)
-console.log(two)
+console.log(one);
+console.log(two);
